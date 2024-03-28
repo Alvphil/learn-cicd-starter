2abb639d8737164bc78e3d9387ed09d4cbea4c9c
Alvphil <philip.nyblom@gmail.com>
2024-03-28 21:23:20 +0100

    Update with working test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestGetAPIKey(t *testing.T) {
 	req, _ := http.NewRequest("GET", "", nil)
-	req.Header.Set("Authorization", "ApiKey")
+	req.Header.Set("Authorization", "ApiKey 123")
 	_, err := GetAPIKey(req.Header)
 	if err != nil {
 		t.Fatalf("%v", err)
